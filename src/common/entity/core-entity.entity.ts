87741b715c6dc640f0b8f1891ac7424e2da0240a
LeeHakJoon <2hakjoon@gmail.com>
2022-02-11 23:22:50 +0900

    add id to walk

--- src/common/entity/core-entity.entity.ts
+++ src/common/entity/core-entity.entity.ts
@@ -1,11 +1,15 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { IsNumber } from 'class-validator';
 import {
   CreateDateColumn,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
+@ObjectType()
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  @Field(() => Int)
   id: number;
 
   @CreateDateColumn()
