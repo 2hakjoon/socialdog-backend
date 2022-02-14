d6a2ce62140b9953f70936efc6ac5ea825b703a5
LeeHakJoon <2hakjoon@gmail.com>
2022-02-14 13:39:34 +0900

    split user entity

--- src/users/entities/users-connection.dto.ts
+++ src/users/entities/users-connection.dto.ts
@@ -1,24 +1,24 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsEmail, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entity/core-entity.entity';
-import { Walks } from 'src/walks/entities/walks.entity';
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity()
 @InputType({ isAbstract: true })
 @ObjectType()
 export class User extends CoreEntity {
-  @Field((type) => String, { nullable: true })
-  @Column({ nullable: true })
+  @Field((type) => String)
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Field((type) => String)
+  @Column({ select: false })
   @IsString()
-  username?: string;
+  password: string;
 
   @Field((type) => String, { nullable: true })
   @Column({ nullable: true })
   @IsString()
-  dogname?: string;
-
-  @Field((type) => [Walks], { nullable: true })
-  @OneToMany(() => Walks, (walk) => walk.user)
-  walks?: Walks[];
+  refreshToken: string;
 }
