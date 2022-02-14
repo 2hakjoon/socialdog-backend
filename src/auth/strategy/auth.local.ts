860a73b2331bf9b8d3076824fa94e5dc9f2ee606
LeeHakJoon <2hakjoon@gmail.com>
2022-02-14 16:25:51 +0900

    error fix

--- src/auth/strategy/auth.local.ts
+++ src/auth/strategy/auth.local.ts
@@ -2,7 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
-import { LoginInputDto, LoginOutputDto } from '../dtos/localLogin.dto';
+import { LoginInputDto, LoginOutputDto } from '../dtos/local-login.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
