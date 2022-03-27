33e85cedffb0efde9bf7f1338e5ab436da03a075
LeeHakJoon <2hakjoon@gmail.com>
2022-03-27 23:54:33 +0900

    add comment entity

---
+++ src/comment/entities/comment.entity.ts
@@ -0,0 +1,34 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entity/core-entity.entity';
+import { Posts } from 'src/posts/entities/posts.entity';
+import { UserProfile } from 'src/users/entities/users-profile.entity';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+
+@Entity()
+@InputType({ isAbstract: true })
+@ObjectType()
+export class Comments extends CoreEntity {
+  @Field((type) => String)
+  @IsString()
+  @Length(0, 300)
+  @Column()
+  contents: string;
+
+  // relations
+  @Field(() => Posts)
+  @ManyToOne(() => Posts, (posts) => posts.id)
+  post: Posts;
+
+  @Field(() => UserProfile)
+  @ManyToOne(() => UserProfile, (user) => user.id)
+  user: UserProfile;
+
+  @Field(() => Comments)
+  @OneToMany(() => Comments, (comment) => comment.childComment)
+  parentComment: Comments;
+
+  @Field(() => Comments)
+  @OneToMany(() => Comments, (comment) => comment.parentComment)
+  childComment: Comments;
+}
