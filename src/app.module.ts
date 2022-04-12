26a6dd9319f72c90b8543cea19e18a6d57de0abc
LeeHakJoon <2hakjoon@gmail.com>
2022-04-12 23:34:07 +0900

    add docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,7 +29,7 @@ import { Comments } from './comments/entities/comments.entity';
   imports: [
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').optional(),
         DB_HOST: Joi.string().required(),
         DB_NAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
