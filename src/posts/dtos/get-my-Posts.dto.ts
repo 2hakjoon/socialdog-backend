98da2e7657f35aa9bb0c3d538c59b808758e37d3
Hak Joon Lee <2hakjoon@gmail.com>
2022-04-26 23:23:35 +0900

    Delete get-my-Posts.dto.ts

--- src/posts/dtos/get-my-Posts.dto.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, ObjectType } from '@nestjs/graphql';
-import { CorePostsOutputDto } from 'src/common/dtos/core-output.dto';
-
-@ObjectType()
-export class GetMyPostsOutputDto extends CorePostsOutputDto {}
