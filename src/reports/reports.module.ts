d4b11955b2c4d728e3c3092557121af68bbdec89
HAKJOON0223 <2hakjoon@gmail.com>
2022-06-09 17:45:20 +0900

    add resolver and service

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportBugs } from './entities/report-bugs.entity';
+import { ReportComments } from './entities/report-comments.entity';
+import { ReportPosts } from './entities/report-posts.entity';
+import { ReportUsers } from './entities/report-users.entity';
+import { ReportsResolver } from './reports.resolver';
+import { ReportsService } from './reports.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ReportBugs,
+      ReportComments,
+      ReportPosts,
+      ReportUsers,
+    ]),
+  ],
+  exports: [ReportsResolver, ReportsService],
+  providers: [ReportsResolver, ReportsService],
+})
 export class ReportsModule {}
